41b710a2cffe3783b6dd7b61992e14e290726fff
Dov Azogui <dov.azogui@gmail.com>
2024-10-06 17:59:47 -0400

    created the functions

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(f"{i}")
+        i -= 1
+    print("Happy New Year!")
+    
+    
+    
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    new_int_list = list()
+    for numbers in int_list:
+        numbers = numbers*numbers
+        new_int_list.append(numbers)
+    return new_int_list
+        
+    
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for n in range(1,101):
+        if n % 3 == 0 and n % 5 == 0:
+            print("FizzBuzz")
+        elif n % 3 == 0:
+            print("Fizz")
+        elif n % 5 == 0:
+            print("Buzz")
+        else: 
+            print(f"{n}")
+        
+
+fizzbuzz()
+
